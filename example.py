ed40214289dfaa75cb65047500b047d448d44d71
Faisal413 <86546153+Faisal413@users.noreply.github.com>
2021-07-08 17:35:10 +0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,8 @@
+from example_pkg import funcaions
+
+
+if __name__ == "__main__":
+    print(functions.lowercase_text('ADVANCED PYTHON PROGRAMMING'))
+    print(functions.remove_punctuation('ADVANCED_PYTHON_PROGRAMMING!!!!'))
+    print(functions.most_common_letters('advanced python programming'))
+    print(functions.merge_dictionaries({"python": 6, "programming": 11}, {"python": 12, "programming": 22}))
